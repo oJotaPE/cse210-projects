071fb97541527f8f6e2f15c03f8503187fcb6e49
João P. <65201780+oJotaPE@users.noreply.github.com>
2024-01-16 14:21:14 -0300

    grades program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Net;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string input = Console.ReadLine();
+
+        int gradeValue = int.Parse(input);
+        string grade = "F";
+
+        if (gradeValue >= 90)
+        {
+            grade = "A";
+        }
+        else if (gradeValue >= 80)
+        {
+            grade = "B";
+        }
+        else if (gradeValue >= 70)
+        {
+            grade = "C";
+        }
+        else if (gradeValue >= 60)
+        {
+            grade = "D";
+        }
+        else if (gradeValue < 60)
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is {grade}");
+
+        string status = "-";
+        if (gradeValue >= 70)
+        {
+            status = "Positive";
+            Console.WriteLine($"Congratulations! your status is {status}");
+        }
+        else
+        {
+            status = "Negative";
+            Console.WriteLine($"Your status is {status}, try again next semester! ");
+        }
+
+        
+
+
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.Write("What's your favorite color? ");
+        string favColor = Console.ReadLine();
+
+        if (favColor == "blue")
+        {
+            Console.WriteLine("You have a good opinion");
+        }
+
+        else if (favColor == "black" || favColor == "purple")
+        {
+            Console.WriteLine("Your opninion makes me sleepy");
+        }
+
+        else
+        {
+            Console.WriteLine("You don't matter for me");
+        }
     }
 }
