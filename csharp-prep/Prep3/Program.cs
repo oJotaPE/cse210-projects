2209e49fb4cdbe8b978750f8aced6b5d0fd9b2ac
João P. <65201780+oJotaPE@users.noreply.github.com>
2024-01-16 20:00:42 -0300

    guess the number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("Enter a number: ");
+        string stringNumber = Console.ReadLine();
+        int number = int.Parse(stringNumber);
+
+        int guess = 999999;
+        do
+        {
+            Console.Write("Enter yout guess: ");
+            string stringGuess = Console.ReadLine();
+            guess = int.Parse(stringGuess);
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess == number)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (guess != number);
     }
 }
