6094208600314c40b1b433e6187c11f9e020dc70
João P. <65201780+oJotaPE@users.noreply.github.com>
2024-01-17 15:20:50 -0300

    min max average

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        // int inputnumber = 99999917;
+        // int total = 0;
+        // int max = 0;
+        // int min = 99999900;
+        int inputnumber;
+        int total = 0;
+        int max = int.MinValue;
+        int min = int.MaxValue;
+        do
+        {
+            Console.Write("Enter a number to the list, type '0' to stop: ");
+            string stringInputNumber = Console.ReadLine();
+            inputnumber = int.Parse(stringInputNumber);
+
+            if (inputnumber != 0)
+            {
+                numbers.Add(inputnumber);
+            }
+            
+        } while(inputnumber != 0);
+
+        int listSize = numbers.Count;
+        Console.WriteLine("numbers in the list:");
+
+        // foreach(int number in numbers)
+        // {
+        //     total = total + number;
+        //     Console.WriteLine(number);
+        //     if (number > max)
+        //     {
+        //         max = number;
+        //     }
+        //     if (number < min)
+        //     {
+        //         min = number;
+        //     }
+        // }
+        foreach (int number in numbers)
+        {
+            total += number;
+            Console.WriteLine(number);
+
+            if (number > max)
+            {
+                max = number;
+            }
+
+            if (number < min)
+            {
+                min = number;
+            }
+        }
+        int averageValue = total / listSize;
+        Console.WriteLine($"this is the total amount: {total}");
+        Console.WriteLine($"this is the average: {averageValue}");
+        Console.WriteLine($"this is the maximum value: {max}");
+        Console.WriteLine($"this is the minimum value: {min}");
+
     }
 }
