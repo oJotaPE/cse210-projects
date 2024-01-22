2221847380a984d41e26fdd0d420c7994996a952
João P. <65201780+oJotaPE@users.noreply.github.com>
2024-01-22 11:38:31 -0300

    okay

--- prove/Develop02/Save.cs
+++ prove/Develop02/Save.cs
@@ -2,37 +2,12 @@ public class Save
 {
     public string filename = "";
 
-    public void CreateFile()
+    public void _CreateFile()
     {
-        Console.WriteLine("Enter a file name for your Journal, do not type the extension or folder to save");
-        Console.Write("> ");
-        string nameForFile = Console.ReadLine();
-        filename = nameForFile;
-
-        if (!string.IsNullOrWhiteSpace(filename))
-        {
-            // Combine the filepath with the filename given by the user
-            string directoryPath = @"C:\Users\Usuario\Documents\cse210-projects-1\prove\Develop02\";
-            string filePath = Path.Combine(directoryPath, filename + ".txt");
-
-            // Check if the directory exists and create if it doesn't
-            if (!Directory.Exists(directoryPath))
-            {
-                Directory.CreateDirectory(directoryPath);
-            }
-
-            // Generate the file
-            File.Create(filePath);
-
-            // Add the question, answer, and date to the same file
-            using (var file = new StreamWriter(filePath, true))
-            {
-                file.WriteLine("test");
-            }
-        }
-        else
-        {
-            Console.WriteLine("Name not found, the file could not be generated.");
-        }
+        
+    }
+    public void _saveOnLoaded()
+    {
+        
     }
 }
