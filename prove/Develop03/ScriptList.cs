e1457e60994941f059d0a3d5a4acd9915aa7e5cc
João P. <65201780+oJotaPE@users.noreply.github.com>
2024-01-29 13:58:44 -0300

    aa

--- prove/Develop03/ScriptList.cs
+++ prove/Develop03/ScriptList.cs
@@ -1,41 +1,4 @@
-// using System.Threading.Channels;
 
-// public class ScriptureList
-
-// {
-//     List<string> john316 = new List<string>
-//     {
-//         "For", "God", "so", "loved", "the", "world", "that", "he", "gave", "his", "only", "Son."
-//     };
-
-//     List<string> james15 = new List<string>
-//     {
-//         "If", "any", "of", "you", "lacks", "wisdom,", "let", "him", "ask", "of", "God,", "who", "gives", "to", "all", "liberally", "and", "without", "reproach,", "and", "it", "will", "be", "given", "to", "him."
-//     };
-
-//     List<string> genesis11 = new List<string>
-//     {
-//         "In", "the", "beginning,", "God", "created", "the", "heavens", "and", "the", "earth."
-//     };
-
-//     List<string> matthew35 = new List<string>
-//     {
-//     "Then", "Jerusalem,", "all", "Judea,", "and", "all", "the", "region", "around", "the", "Jordan", "went", "out", "to", "him"
-//     };
-
-//     List<string> luke24to6 = new List<string>
-//     {
-//         "Joseph", "also", "went", "up", "from", "Galilee,", "out", "of", "the", "city", "of", "Nazareth,", "into", "Judea,", "to", "the", "city", "of", "David,", "which", "is", "called", "Bethlehem,", "because", "he", "was", "of", "the", "house", "and", "family", "of", "David,",
-//         "to", "be", "registered", "with", "Mary,", "his", "betrothed", "wife,", "who", "was", "with", "child.",
-//         "So", "it", "was,", "that", "while", "they", "were", "there,", "the", "days", "were", "completed", "for", "her", "to", "be", "delivered."
-//     };
-
-//     public List<string> ReturnWords()
-//     {
-//         List<String> finalList = luke24to6;
-//         return finalList;
-//     }
-// }
 
 public class ScriptureList
 {
