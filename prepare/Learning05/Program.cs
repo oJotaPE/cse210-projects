e803e0c23738bf83895f45043f42cf3648e386dc
João P. <65201780+oJotaPE@users.noreply.github.com>
2024-02-08 11:23:37 -0300

    a

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square s1 = new Square("green", 15);
+        Rectangle s2 = new Rectangle("yellow", 10, 23);
+        Circle s3 = new Circle("purple", 14);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(s1);
+        shapes.Add(s2);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+        
     }
 }
---
+++ prepare/Learning05/Square.cs
@@ -0,0 +1,17 @@
+using System.Drawing;
+
+public class Square : Shape
+{
+    private double _side ;
+
+    public Square(string color, double side) : base (color)
+    {
+        _side = side;
+    }
+
+    public override double GetArea()
+    {
+        double area = _side * 4;
+        return area;
+    }
+}
